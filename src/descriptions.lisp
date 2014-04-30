985de784bf4d6cf530e0282596e9bf571da6c354
mmontone <marianomontone@gmail.com>
2014-04-29 21:21:21 -0300

    Bug fix

--- src/descriptions.lisp
+++ src/descriptions.lisp
@@ -1,59 +1,102 @@
 (in-package :{})
 
 ;; Syntax and api
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defun symbol->keyword (symbol)
+    (intern (symbol-name symbol) :keyword))
+  
+  (defmacro define-description (name parents attributes &rest options)
+    "Define a new description"
+    (flet ((parse-description-attribute (attribute-spec)
+	     (destructuring-bind (attribute-name
+				  attribute-type
+				  &rest initargs) attribute-spec
+	       `(make-attribute ,attribute-type :name ',attribute-name
+				,@initargs))))
+      `(progn
+	 ;; The static description definition
+	 (defproto ,name ,(if parents
+			      parents
+			      (list '{description}))
+	 
+	   ,(cons `(description-name ',name)
+		  (loop for attribute in attributes
+		     collect (list (first attribute) (parse-description-attribute attribute) :accessor nil)))
+	   ,@options)
+       
+	 ,(when (getf options :documentation)
+		`(setf (documentation ',name 'variable)
+		       ,(getf options :documentation)))
+
+	 ;; A description builder function
+	 (defun ,name (&rest parents)
+	   ,(format nil "Create a ~A description. Takes a list of descriptions to use as parents of the new description." name)
+	   (make-description :parents (or parents
+					  ,(if parents
+					       `(list ,@parents)
+					       '(list {description})))
+			     :attributes
+			     (list ,@(loop
+					for attribute in attributes
+					collect `(list ',(first attribute)
+						       ,(parse-description-attribute attribute)))))))))
+
+  (defmacro with-description-attributes (attributes description &body body)
+    "Run body with description attributes bound"
+    `(let ,(loop for attribute in attributes
+	      collect
+		(list attribute `(get-attribute ,description ',attribute)))
+       ,@body))
+
+  (defmacro define-attribute (name parents properties &rest options)
+    "Define an attribute type
+
+:param name: the attribute type name.
+:param parents: the attribute type parents.
+:param properties: list of properties.
+:param options: options, like :documentation, etc"
+  
+    (flet ((parse-attribute-property (property-spec)
+	     (destructuring-bind (property-name property-value &key reader writer accessor)
+		 property-spec
+	       `(,(symbol->keyword property-name)
+		  ,property-value
+		  ,@(when reader
+			  `(:reader ',reader))
+		  ,@(when writer
+			  `(:writer ',writer))
+		  ,@(when accessor
+			  `(:accessor ',accessor))))))
+      `(progn
+	 (defproto ,name ,(if parents
+			      parents
+			      (list '=>))
+	   ,(loop for property in properties
+	       collect (parse-attribute-property property))
+	   ,@options)
+	 ,(when (getf options :documentation)
+		`(setf (documentation ',name 'variable)
+		       ,(getf options :documentation)))
+	 (defun ,name (&rest property-values)
+	   ,(format nil "Create a ~A attribute. Takes a plist of property values for the created attribute" name)
+	   (apply #'make-attribute ,name property-values)))))
+  )
 
 (defun make-attribute (attribute-type &rest property-values)
-  "Create an attribute."
-  
-  (flet ((plist-to-properties (property-values)
-	   (loop
-	      for prop in property-values by #'cddr
-	      for value in (cdr property-values) by #'cddr
-	      collect (list (if (keywordp prop)
-				(intern (symbol-name prop))
-				prop)
-			    value))))
-    (object :parents (if (listp attribute-type)
-			 attribute-type
-			 (list attribute-type))
-	    :properties (cons (list 'type attribute-type)
-			      (plist-to-properties property-values)))))
+    "Create an attribute.
 
-(defmacro define-description (name parents attributes &rest options)
-  "Define a new description"
-  (flet ((parse-description-attribute (attribute-spec)
-	   (destructuring-bind (attribute-name
-				attribute-type
-				&rest initargs) attribute-spec
-	     `(make-attribute ,attribute-type :name ',attribute-name
-			      ,@initargs))))
-    `(progn
-       ;; The static description definition
-       (defproto ,name ,(if parents
-			    parents
-			    (list '{description}))
-	 
-	 ,(cons `(description-name ',name)
-		(loop for attribute in attributes
-		   collect (list (first attribute) (parse-description-attribute attribute) :accessor nil)))
-	 ,@options)
-       
-       ,(when (getf options :documentation)
-	      `(setf (documentation ',name 'variable)
-		     ,(getf options :documentation)))
-
-       ;; A description builder function
-       (defun ,name (&rest parents)
-	 ,(format nil "Create a ~A description. Takes a list of descriptions to use as parents of the new description." name)
-	 (make-description :parents (or parents
-					,(if parents
-					     `(list ,@parents)
-					     '(list {description})))
-			   :attributes
-			   (list ,@(loop
-				      for attribute in attributes
-				      collect `(list ',(first attribute)
-						     ,(parse-description-attribute attribute)))))))))
+:param attribute-type: The attribute type
+:property-values: A plist of attribute properties values
+"
+    (flet ((plist-properties (plist)
+	     (loop for key in plist by #'cddr
+		for value in (cdr plist) by #'cddr
+		collect (list key value))))
+      (object :parents (if (listp attribute-type)
+			   attribute-type
+			   (list attribute-type))
+	      :properties (cons (list 'type attribute-type)
+				(plist-properties property-values)))))
 
 (defun description-attributes (description)
   "Obtain a description attributes.
@@ -63,45 +106,6 @@
           (remove-if (lambda (prop)
                        (member prop (list 'description-name 'sheeple::nickname 'documentation)))
                      (available-properties description))))
-
-(defmacro with-description-attributes (attributes description &body body)
-  "Run body with description attributes bound"
-  `(let ,(loop for attribute in attributes
-	      collect
-	      (list attribute `(get-attribute ,description ',attribute)))
-     ,@body))
-
-(defmacro define-attribute (name parents properties &rest options)
-  "Define an attribute type
-
-:param name: the attribute type name.
-:param parents: the attribute type parents.
-:param properties: list of properties.
-:param options: options, like :documentation, etc"
-  
-  (flet ((parse-attribute-property (property-spec)
-	   (destructuring-bind (property-name property-value &key reader writer accessor)
-	       property-spec
-	     `(,property-name ,property-value
-			      ,@(when reader
-				      `(:reader ',reader))
-			      ,@(when writer
-				      `(:writer ',writer))
-			      ,@(when accessor
-				      `(:accessor ',accessor))))))
-    `(progn
-       (defproto ,name ,(if parents
-			  parents
-			  (list '=>))
-       ,(loop for property in properties
-	   collect (parse-attribute-property property))
-       ,@options)
-       ,(when (getf options :documentation)
-	      `(setf (documentation ',name 'variable)
-		     ,(getf options :documentation)))
-       (defun ,name (&rest property-values)
-	 ,(format nil "Create a ~A attribute. Takes a plist of property values for the created attribute" name)
-	 (apply #'make-attribute ,name property-values)))))
 
 ;; Attributes
 
